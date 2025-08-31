79b7392e206f60a845f4a5536dc14463fef0263e
nlarralde13 <nlarralde@gmail.com>
2025-08-31 12:58:38 -0400

    Add Town model and migration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -6,10 +6,12 @@ from .characters import Character            # noqa: F401
 from .items import Item                      # noqa: F401
 from .inventory import ItemInstance, CharacterInventory   # noqa: F401
 # from .crafting import Recipe               # noqa: F401
+from .towns import Town                     # noqa: F401
 
 __all__ = [
     "db", "Model", "metadata",
     "User", "Character",
     "Item", "ItemInstance", "CharacterInventory",
+    "Town",
     # "Recipe",
 ]
